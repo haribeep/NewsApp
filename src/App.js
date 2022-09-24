36aa81b4195d98d499f392daaf34ab80a14c20d6
hari <harimurali0070@gmail.com>
2022-09-24 19:04:58 +0530

    Navbar created and react-router-dom

--- src/App.js
+++ src/App.js
@@ -1,9 +1,16 @@
 import './App.css';
+import {BrowserRouter as Router,Route,Routes} from 'react-router-dom'
+import LoadingBar from 'react-top-loading-bar'
+import Navbar from './component/Navbar';
 
 function App() {
   return (
     <div className="App">
      App
+    <Router>
+      <Navbar/>
+
+    </Router>
     </div>
   );
 }
